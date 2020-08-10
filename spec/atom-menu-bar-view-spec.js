a284ff9b8309b7cc4a22ef0c12760a429304e400
bulby <benharless820@gmail.com>
2020-08-10 11:45:40 -0400

    Create repository

---
+++ spec/atom-menu-bar-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomMenuBarView from '../lib/atom-menu-bar-view';
+
+describe('AtomMenuBarView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
