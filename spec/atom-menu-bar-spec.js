d6d3c473de035dce75b6ad412412dfff73d576a7
bulby <benharless820@gmail.com>
2020-08-14 18:39:14 -0400

    Add specs; Use Standard JS Style

--- spec/atom-menu-bar-spec.js
+++ spec/atom-menu-bar-spec.js
@@ -1,6 +1,7 @@
-'use babel';
+/* eslint-env jasmine, node */
+'use babel'
 
-import AtomMenuBar from '../lib/atom-menu-bar';
+import AtomMenuBar from '../lib/atom-menu-bar'
 
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
 //
@@ -8,66 +9,8 @@ import AtomMenuBar from '../lib/atom-menu-bar';
 // or `fdescribe`). Remove the `f` to unfocus the block.
 
 describe('AtomMenuBar', () => {
-  let workspaceElement, activationPromise;
-
-  beforeEach(() => {
-    workspaceElement = atom.views.getView(atom.workspace);
-    activationPromise = atom.packages.activatePackage('atom-menu-bar');
-  });
-
-  describe('when the atom-menu-bar:toggle event is triggered', () => {
-    it('hides and shows the modal panel', () => {
-      // Before the activation event the view is not on the DOM, and no panel
-      // has been created
-      expect(workspaceElement.querySelector('.atom-menu-bar')).not.toExist();
-
-      // This is an activation event, triggering it will cause the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'atom-menu-bar:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        expect(workspaceElement.querySelector('.atom-menu-bar')).toExist();
-
-        let atomMenuBarElement = workspaceElement.querySelector('.atom-menu-bar');
-        expect(atomMenuBarElement).toExist();
-
-        let atomMenuBarPanel = atom.workspace.panelForItem(atomMenuBarElement);
-        expect(atomMenuBarPanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 'atom-menu-bar:toggle');
-        expect(atomMenuBarPanel.isVisible()).toBe(false);
-      });
-    });
-
-    it('hides and shows the view', () => {
-      // This test shows you an integration test testing at the view level.
-
-      // Attaching the workspaceElement to the DOM is required to allow the
-      // `toBeVisible()` matchers to work. Anything testing visibility or focus
-      // requires that the workspaceElement is on the DOM. Tests that attach the
-      // workspaceElement to the DOM are generally slower than those off DOM.
-      jasmine.attachToDOM(workspaceElement);
-
-      expect(workspaceElement.querySelector('.atom-menu-bar')).not.toExist();
-
-      // This is an activation event, triggering it causes the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'atom-menu-bar:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        // Now we can test for view visibility
-        let atomMenuBarElement = workspaceElement.querySelector('.atom-menu-bar');
-        expect(atomMenuBarElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 'atom-menu-bar:toggle');
-        expect(atomMenuBarElement).not.toBeVisible();
-      });
-    });
-  });
-});
+  it('attaches a menu bar to the top of the workspace.', () => {
+    expect(AtomMenuBar.atomMenuBarView.getElement().hidden).toBe(false)
+    expect(AtomMenuBar.atomMenuBarView.getElement().closest('*')).toBe(document.querySelector('atom-panel.header.tool-panel.panel-header'))
+  })
+})
--- spec/atom-menu-bar-view-spec.js
+++ spec/atom-menu-bar-view-spec.js
@@ -1,9 +1,10 @@
-'use babel';
+/* eslint-env jasmine, node */
+'use babel'
 
-import AtomMenuBarView from '../lib/atom-menu-bar-view';
+// import AtomMenuBarView from '../lib/atom-menu-bar-view'
 
 describe('AtomMenuBarView', () => {
   it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
+    expect('life').toBe('easy')
+  })
+})
